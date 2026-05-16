adf1ebfbf7eda420334faa38b7fc0bffbccc8fdc
Cleboost <clement.balarot@gmail.com>
2026-05-17 01:09:40 +0200

    refactor(instrumentation): add noinspection directive for unused symbols

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,3 +1,4 @@
+// noinspection JSUnusedGlobalSymbols
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { runMigrations } = await import('./lib/db/migrate');
